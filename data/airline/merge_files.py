a49307788027fde774a241af811e6299d8634ab2
Karl Krauth <karl.krauth@gmail.com>
2016-06-10 15:33:59 +1000

    Updated airline pre-processing scripts to work with original data.

--- data/airline/merge_files.py
+++ data/airline/merge_files.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
+# Read in flight data and keep months of Jan-April.
+flights = pd.read_csv('2008.csv')
+flights = flights[(flights['Month'] >= 1) & (flights['Month'] <= 4)]
 
-res1 = pd.read_csv('result.csv')
-res2 = pd.read_csv('plane-data.csv')[['TailNum', 'plane_year']].replace('None', np.nan).dropna()
-res = pd.merge(res1, res2, on='TailNum')
+# Read in plane data and drop invalid values/change headers.
+planes = pd.read_csv('plane-data.csv')[['tailnum', 'year']].replace('None', np.nan).dropna()
+planes.columns = ['TailNum', 'plane_year']
+
+# Merge the two files and output the result.
+res = pd.merge(flights, planes, on='TailNum')
 res.to_csv('temp.csv')
--- data/airline/read_airline.py
+++ data/airline/read_airline.py
@@ -8,16 +8,17 @@ data_filtered = data[['DayOfWeek', 'AirTime', 'Distance', 'plane_year', 'DepTime
 actual_delays = data_filtered['ArrDelay']>0
 data_filtered = data_filtered[actual_delays]
 X = data_filtered[['DayOfWeek', 'AirTime', 'Distance', 'plane_year', 'DepTime', 'ArrTime', 'DayofMonth', 'Month']].values
-# X = data_filtered[['Distance']].values
 Y = data_filtered['ArrDelay'].values[:, None]
 print X.shape, Y.shape
 
 # Transform plane deployment year in plane age
 X[:, 3] = 2008 - X[:, 3]
+
+# Transform timestamps to hour values.
 X[:, 4] = np.floor_divide(X[:, 4], 100) + np.mod(X[:, 4], 100) / 60.0
 X[:, 5] = np.floor_divide(X[:, 5], 100) + np.mod(X[:, 5], 100) / 60.0
 
-# 2. Holdout sets
+# Split the data into test and training set.
 N, D = X.shape
 num_test = 100000# N*0.10
 num_train = 700000
@@ -27,18 +28,6 @@ train = N_shuffled[num_test:num_test + num_train]
 X_train, Y_train = X[train], Y[train]
 X_test, Y_test = X[test], Y[test]
 
-# print data_filtered.columns.values
-# print "train", X_train.mean(0)
-# print "test", X_test.mean(0)
-# print Y_train.mean(), Y_test.mean()
-
+# Output test and training data.
 np.savetxt("train.csv", np.concatenate([X_train, Y_train], 1), delimiter=",")
 np.savetxt("test.csv", np.concatenate([X_test, Y_test], 1), delimiter=",")
-
-# Xmean = X_train.mean(0)
-# Xstd = X_train.std(0)
-# X_train = (X_train-Xmean)/Xstd
-
-# Ymean = Y_train.mean()
-# Ystd = Y_train.std()
-# Y_train = (Y_train-Ymean)/Ystd
