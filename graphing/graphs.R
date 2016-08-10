bed7f1918d2d796abb1e2fbf2fbebfc45ddf31fd
Karl Krauth <karl.krauth@gmail.com>
2016-08-10 17:02:07 +1000

    Updated graphing for airline.

--- graphing/graphs.R
+++ graphing/graphs.R
@@ -20,10 +20,39 @@ source('helper.R')
 
 SP_name = "SF"
 
-w = 15
-h = 7
+w = 12
+h = 4.5
 output_path = "./"
 
+merge_figs = function(p1, p2)
+{
+  grid.newpage()
+
+
+# extract gtable
+  g1 <- ggplot_gtable(ggplot_build(p1))
+  g2 <- ggplot_gtable(ggplot_build(p2))
+  
+  # overlap the panel of 2nd plot on that of 1st plot
+  pp <- c(subset(g1$layout, name == "panel", se = t:r))
+  g <- gtable_add_grob(g1, g2$grobs[[which(g2$layout$name == "panel")]], pp$t, 
+                       pp$l, pp$b, pp$l)
+  
+  # axis tweaks
+  ia <- which(g2$layout$name == "axis-l")
+  ga <- g2$grobs[[ia]]
+  ax <- ga$children[[2]]
+  ax$widths <- rev(ax$widths)
+  ax$grobs <- rev(ax$grobs)
+  g <- gtable_add_cols(g, g2$widths[g2$layout[ia, ]$l], length(g$widths) - 1)
+  g <- gtable_add_grob(g, ax, pp$t, length(g$widths) - 1, pp$b)
+  
+  # draw it
+  p1 = grid.draw(g)
+  g
+}
+
+#if (FALSE) {
 # boston data
 data = read.csv('../../graph_data/boston_SSE_data.csv')
 p1 = draw_boxplot_models_with_X(data, "SSE", "None")
@@ -163,7 +192,7 @@ data = cbind(standard_data, inducing_data)
 p2 = draw_boxplot_models_with_X(data, "NLPD", "None")
 ggsave(file=paste(output_path, 'sarcos_inducing_NLPD', ".pdf", sep = ""),  width=w/2, height=h, units = "cm" , device=cairo_pdf, p2)
 
-# sarcos all joints inducing data
+# sarcosi all joints inducing data
 inducing_data = read.csv('../../graph_data/sarcos_all_joints_MSSE_data_inducing.csv')
 standard_data = read.csv('../../graph_data/sarcos_all_joints_MSSE_data.csv')
 standard_data = standard_data[, -grep("0\\.00[14]", colnames(standard_data))]
@@ -177,3 +206,133 @@ standard_data = standard_data[, -grep("0\\.00[14]", colnames(standard_data))]
 data = cbind(standard_data, inducing_data)
 p2 = draw_boxplot_models_with_X(data, "NLPD", "None")
 ggsave(file=paste(output_path, 'sarcos_all_joints_inducing_NLPD', ".pdf", sep = ""),  width=w/2, height=h, units = "cm" , device=cairo_pdf, p2)
+
+#for ailine data
+lin_RMSE = c(38.5742766187, 40.8273352476, 34.578759036, 36.7876864506, 45.3542697617)
+
+lin_NLPD = c(5.08685873604, 5.13457483244, 4.97758451918, 5.02466269926, 5.27594624659)
+
+
+output = list()
+index = 1
+for(i in 0:4){
+  nlpd = read.csv(paste("../../graph_data/airline/airline", i, "/nlpd.txt", sep=""), header = F)
+  rmse = read.csv(paste("../../graph_data/airline/airline", i, "/rmse.txt", sep=""), header = F)
+  obj = read.csv(paste("../../graph_data/airline/airline", i, "/obj.txt", sep=""), header = F)
+  output[[index]] = data.frame(nlpd = as.vector(nlpd[,1]), rmse = as.vector(rmse[,1]), obj = as.vector(obj[,1]),
+                               expr = i, 
+                               method="SAVIGP", epoch=1:nrow(nlpd))
+  index = index + 1
+  nlpd = read.csv(paste("../../graph_data/airline/airline", i, "/nlpdsvi.txt", sep=""), header = F)
+  rmse = read.csv(paste("../../graph_data/airline/airline", i, "/rmsesvi.txt", sep=""), header = F)
+  output[[index]] = data.frame(nlpd = as.vector(nlpd[,1]), rmse = as.vector(rmse[,1]), obj=NA, expr = i, 
+                               method="SVI", epoch=1:nrow(nlpd))
+  total_epochs = nrow(nlpd)
+  index = index + 1
+  
+  nlpd = read.csv(paste("../../graph_data/airline/airline", i, "/nlpdlargegp.txt", sep=""), header = F)
+  rmse = read.csv(paste("../../graph_data/airline/airline", i, "/rmselargegp.txt", sep=""), header = F)
+  output[[index]] = data.frame(nlpd = mean(nlpd[,1]), rmse = mean(rmse[,1]), obj=NA, expr = i, 
+                               method="GP2000", epoch=1:total_epochs)
+  index = index + 1
+  
+  nlpd = read.csv(paste("../../graph_data/airline/airline", i, "/nlpdsmallgp.txt", sep=""), header = F)
+  rmse = read.csv(paste("../../graph_data/airline/airline", i, "/rmsesmallgp.txt", sep=""), header = F)
+  output[[index]] = data.frame(nlpd = mean(nlpd[,1]), rmse = mean(rmse[,1]), obj=NA, expr = i, 
+                               method="GP1000", epoch=1:total_epochs)
+  index = index + 1
+  
+  output[[index]] = data.frame(nlpd = lin_NLPD[i+1], rmse = lin_RMSE[i+1], obj=NA, expr = i, 
+                               method="LINEAR", epoch=1:total_epochs)
+  index = index + 1
+  
+}
+require(data.table)
+res = rbindlist(output)
+
+require(ggplot2)
+draw_line_chart = function(res, ylabel, legend="none") {
+  p1 = ggplot(res, aes(x=epoch, y = y, color = method)) +
+  stat_summary(fun.y = "mean", geom="line", size=1) +
+  theme_bw() +
+  scale_color_brewer(palette="Set1", name = "") +
+  theme_bw() +
+  xlab("epoch") +
+  ylab(ylabel) +
+  scale_x_continuous(expand = c(0, 0)) + 
+  theme(legend.direction = "vertical", legend.box = "vertical", legend.position=legend,
+        axis.line.x = element_line(colour = "black", size=0.5),
+        axis.line.y = element_line(colour = "black", size=0.5),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        text=element_text(family="Arial", size=10),
+        panel.border = element_rect(color = "black", fill = NA, size = 0.0),
+        legend.key = element_blank(),
+        panel.background = element_rect(fill = NA)
+  )+
+  guides(fill = guide_legend(keywidth = 0.5, keyheight = 0.5))
+  p1
+}
+
+
+
+res$y = res$nlpd
+p1 = draw_line_chart(res, "NLPD", legend = "right")
+ggsave(file=paste(output_path, 'airline_NLPD', ".pdf", sep = ""),  
+       width=w/1.5, height=h, units = "cm" , device=cairo_pdf, p1)
+
+res$y = res$rmse
+p1 = draw_line_chart(res, "RMSE", legend = "none")
+ggsave(file=paste(output_path, 'airline_RMSE', ".pdf", sep = ""),  
+       width=w/2, height=h, units = "cm" , device=cairo_pdf, p1)
+
+library(gtable)
+library(grid)
+
+draw_line_chart2 = function(res, ylabel, color, name) {
+  p1 = ggplot(res, aes(x=epoch, y = y, color=method)) +
+    stat_summary(fun.y = "mean", geom="line", size=1, color=color) +
+    theme_bw() +
+    scale_color_brewer(palette="Set1", name = "") +
+    theme_bw() +
+    xlab("epoch") +
+    ylab(ylabel) +
+    scale_x_continuous(expand = c(0, 0)) + 
+    theme_bw()+
+    theme(legend.direction = "vertical", legend.box = "vertical",legend.position = c(0.8, 0.2),
+          axis.line.x = element_line(colour = "black", size=0.5),
+          axis.line.y = element_line(colour = "black", size=0.5),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          text=element_text(family="Arial", size=10),
+          panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+          legend.key = element_blank(),
+          panel.background = element_rect(fill = NA),
+          axis.ticks.y = element_blank()
+    )
+  p1
+}
+
+
+res$y = res$obj
+p1 = draw_line_chart2(subset(res, method=="SAVIGP"), "NELBO", "blue")
+
+res$y = res$rmse
+p2 = draw_line_chart2(subset(res, method=="SAVIGP"), "RMSE", "red")
+
+g = merge_figs(p1, p2)
+ggsave(file=paste(output_path, 'airline_NELBO_RMSE', ".pdf", sep = ""),  
+       width=w/1.7, height=h, units = "cm" , device=cairo_pdf, g)
+
+
+res$y = res$obj
+p1 = draw_line_chart2(subset(res, method=="SAVIGP"), "NELBO", "blue")
+
+res$y = res$nlpd
+p2 = draw_line_chart2(subset(res, method=="SAVIGP"), "NLPD", "red")
+
+g = merge_figs(p1, p2)
+ggsave(file=paste(output_path, 'airline_NELBO_NLPD', ".pdf", sep = ""),  
+       width=w/1.7, height=h, units = "cm" , device=cairo_pdf, g)
+
+
