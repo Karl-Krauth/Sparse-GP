2c1f890b73dac694583c73e8a1cf99c6f07ac80a
Karl Krauth <karl.krauth@gmail.com>
2016-08-12 16:26:30 +1000

    Added documentation for wrapper.

--- src/savigp.py
+++ src/savigp.py
@@ -9,6 +9,41 @@ from diagonal_gaussian_process import DiagonalGaussianProcess
 from full_gaussian_process import FullGaussianProcess
 
 class Savigp(object):
+    """
+    A scikit-like class which allows for predictions using scalable automated variational
+    inference on Gaussian process models.
+
+    Parameters
+    ----------
+    likelihood : subclass of Likelihood
+        An object representing the likelihood function. See likelihood.py for the methods
+        such an object needs to implement.
+    kernels : list of kernels
+        A list containing a kernel for each latent process. See kernel.py for the methods each
+        element in the list needs to implement.
+    posterior : str
+        A string denoting the shape of the posterior distribution. Possible values include:
+        'full' (a single Gaussian with a full covariance matrix), 'mix1' (a single Gaussian
+        with a diagonal covariance matrix), and 'mix2' (a mixture of two Gaussians with
+        diagonal covariance matrices).
+    num_inducing : int
+        The number of inducing points to use in training.
+    random_inducing: boolean
+        Whether to put inducing points randomly on training data. If False, inducing points will be
+        determined using clustering.
+    num_samples : int
+        The number of samples used to approximate gradients and objective functions.
+    latent_noise : float
+        The amount of latent noise that will be added to each kernel.
+    partition_size : int
+        How large each partition of the training data will be when calculating expected log
+        likelihood. In the case of batch optimization this is simply used to save on memory;
+        smaller partitions lead to less memory usage but slower runtime. In the case of
+        stochastic optimization, this will also denote the size of the mini-batches used in
+        training.
+    denbug_output : bool
+        Whether to print the value of the objective function as it gets optimized.
+    """
     def __init__(self,
                  likelihood,
                  kernels,
@@ -46,6 +81,29 @@ class Savigp(object):
             param_threshold=1e-3,
             objective_threshold=5,
             max_iterations=200):
+        """
+        train_inputs : ndarray
+            A matrix containing the input training data. Dimensions: num_data_points * input_dim.
+        train_outputs : ndarray
+            A matrix containing the output training data. Dimensions: num_data_points * num_latent.
+        optimization_config : dict
+            The maximum number of function evaluations per subset of parameters for each local
+            optimization. Valid keys are: 'mog' (variational parameters), 'hyp' (kernel
+            hyperparameters), 'll' (likelihood parameters), 'inducing' (inducing point locations).
+            If a set of parameters is not included it will not get optimized.
+        optimize_stochastic : bool
+            Whether to optimize the model stochastically. If set to false batch optimization will
+            be used.
+        param_threshold : float
+            The tolerance threshold for convergence of the variational parameters. The model
+            is considered to have converged if (means_difference + covariances_difference) / 2 <
+            param_threshold.
+        objective_threshold : float
+            The tolerance threshold for convergence of the objective function.
+        max_iterations : int
+            The maximum number of global optimisations. If max_iterations is None the model will
+            keep getting optimized until convergence.
+        """
         # Initialize the model. We should move this to init once we fix the API design issue.
         if self.posterior == 'full':
             self.model = FullGaussianProcess(train_inputs,
@@ -94,4 +152,26 @@ class Savigp(object):
         return total_evals
 
     def predict(self, test_inputs, test_outputs=None):
+        """
+        Make predictions on test inputs and computes the negative log predictive density for the
+        test outputs if they are given.
+
+        Parameters
+        ----------
+        test_inputs : ndarray
+            Dimensions: num_test * input_dim.
+        test_outputs : ndarray
+            Dimensions: num_test * output_dim.
+
+        Returns
+        -------
+        predicted_values : ndarray
+            Predicted values for each test point. Dimensions : num_test * output_dim.
+        predicted_variance : ndarray
+            Predicted variance of the values. Dimensions : num_test * output_dim
+        nlpd : ndarray
+            The negative log predictive density for each test point or None if test outputs aren't
+            provided. Dimensions: num_test * output_dim.
+        """
+
         return self.model.predict(test_inputs, test_outputs)
