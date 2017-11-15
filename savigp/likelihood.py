corrected sign of ll for seismic

savigp/likelihood.py
@@ -587,7 +587,7 @@ class SeismicLL(Likelihood):
         for p in range(1, self.P):
             G[:, :, p] = G[:, :, p-1] + 2 * (depth[:, :, p] - depth[:, :, p - 1]) / vel[:, :, p]
 
-        return ((G - Y) ** 2).sum(axis=2), None
+        return -((G - Y) ** 2).sum(axis=2), None
 
     def get_params(self):
         return np.array(np.log([self.sigma]))