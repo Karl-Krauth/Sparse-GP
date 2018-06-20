3767d53e9ee65f89a8838d8f96d2af1981ba7898
adezfouli <akdezfuli@gmail.com>
2018-06-20 22:12:33 +1000

    updated seismic stan

--- experiments/seismic_stan/seismic.R
+++ experiments/seismic_stan/seismic.R
@@ -11,16 +11,23 @@ x = data[, 5]
 
 N <- length(x)
 
-D <- nrow(y)
+P <- nrow(y)
 
-mu_prior = c(200, 500, 1600, 2200, 1950, 2300, 2750, 3650) * 0
+mu_prior = c(200, 500, 1600, 2200, 1950, 2300, 2750, 3650)
 var_prior = c(900, 5625, 57600, 108900, 38025, 52900, 75625, 133225)
+sigma2y = c(0.0006, 0.0025, 0.0056, 0.0100)
 
-y = list(as.numeric(y[1]), as.numeric(y[2]), as.numeric(y[3]), as.numeric(y[4]))
-
-gp_mult <- stan(file = 'seismic.stan', data = list(x = as.numeric(x), D = D, N = N, N_latent=8, y=y, 
+gp_mult <- stan(file = 'seismic.stan', data = list(x = as.numeric(x), P = P, N = N, N_latent=8, y=y, 
                                                    mu_prior=mu_prior,
-                                                   var_prior = var_prior
-                                                   ), chains = 3, iter = 200, seed = 123)
+                                                   var_prior = var_prior,
+                                                   sigma2y = sigma2y
+                                                   ), chains = 1, iter = 200, seed = 123)
 gp_mult_ss <- rstan::extract(gp_mult)
+f <- gp_mult_ss$f
+
+f_mean = apply(f, c(2,3), mean)
+f_mean[1,1:113]
+dim(f_mean)
+typeof(f)
+f[200,]
 y <- gp_mult_ss$y[200,]
--- experiments/seismic_stan/seismic.stan
+++ experiments/seismic_stan/seismic.stan
@@ -1,11 +1,12 @@
 data {
     int<lower=1> N;
-    int<lower=1> D;
+    int<lower=1> P;
     int<lower=1> N_latent;
     real x[N];
-    vector[N] y[D];
+    vector[N] y[P];
     vector[N_latent] mu_prior;
     vector[N_latent] var_prior;
+    vector[P] sigma2y;
 }
 
 transformed data {
@@ -15,15 +16,22 @@ parameters {
 }
 
 model {
+    int vel_ix = 4;
     matrix[N, N] K[N_latent];
     vector[N] mu[N_latent];
+    vector[N] g[P];
     for (k in 1:N_latent){
-        K[k] = cov_exp_quad(x, var_prior[k], 1.0) + diag_matrix(rep_vector(1e-5, N));
+        K[k] = cov_exp_quad(x, sqrt(var_prior[k]), 1.0) + diag_matrix(rep_vector(1e-5, N));
         mu[k] = rep_vector(mu_prior[k], N);
         f[k] ~ multi_normal(mu[k], K[k]);
     };
     
-    for (k in 1:4){
-        y[k] ~ normal(f[k], rep_vector(0.01, N));
+    g[1] = 2 * f[1] ./ f[1 + vel_ix];
+    for (p in 2:P){
+        g[p] = 2 * (f[p] - f[p - 1]) ./ f[ p + vel_ix] + g[p-1] ;
+    };
+    
+    for (p in 1:P){
+        y[p] ~ normal(g[p], rep_vector(sqrt(sigma2y[p]), N));
     };
 }
