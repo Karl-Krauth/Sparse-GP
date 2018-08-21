78537c06887bffb9cc11019d199b6182af6f9bf9
adezfouli <akdezfuli@gmail.com>
2018-08-21 15:14:45 +1000

    added mcmc analysis file

--- experiments/seismic_stan/mcmc_analysis.R
+++ experiments/seismic_stan/mcmc_analysis.R
@@ -2,8 +2,10 @@ load('seismic.RData')
 gp_mult_ss <- rstan::extract(gp_mult)
 f <- gp_mult_ss$f
 
-f_mean = apply(f, c(2,3), mean)
-f_mean[4,1:113]
+dim(f[10000:12000,,])
+f_mean = apply(f[10000:12000,,], c(2,3), mean)
+dim(f_mean)
+f_mean[4:113]
 dim(f_mean)
 typeof(f)
 f[200,]
